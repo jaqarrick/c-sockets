430c3215c0fae84d4c10df7be92ffa15e2df0272
jaqarrick <jcarrick@protonmail.com>
2024-11-13 16:56:04 -0500

    multi-threaded example

--- examples/port-scanner/multi-threaded.c
+++ examples/port-scanner/multi-threaded.c
@@ -10,33 +10,133 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
+#define NUM_THREADS 16
+#define INIT_QUEUE_CAPACITY 256
 
+typedef struct
+{
+    const char *ip;
+    int start_port;
+    int end_port;
+} ScannerArgs;
+
+ScannerArgs scanner_args;
+
+void validate_port(int port)
+{
+    if (port < 1 || port > 65535)
+    {
+        printf("Invalid port number: %d. Port number must be between 1 and 65535.\n", port);
+        exit(1);
+    }
+}
+
+void validate_args(int argc, char *argv[], ScannerArgs *scanner_args)
+{
+    if (argc < 4)
+    {
+        printf("Usage: %s <IP> <start_port> <end_port>\n", argv[0]);
+        exit(1);
+    }
+
+    scanner_args->ip = argv[1];
+    scanner_args->start_port = atoi(argv[2]);
+    scanner_args->end_port = atoi(argv[3]);
+
+    validate_port(scanner_args->start_port);
+    validate_port(scanner_args->end_port);
+
+    if (scanner_args->end_port < scanner_args->start_port)
+    {
+        printf("Invalid port numbers. Starting port must be less than end port\n");
+        exit(1);
+    }
+}
+
+int scan_port(int port)
+{
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    if (inet_pton(AF_INET, scanner_args.ip, &server_address.sin_addr) <= 0)
+    {
+        printf("Invalid address/ Address not supported\n");
+        return 1;
+    }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        printf("Socket creation error\n");
+        return 1;
+    }
+
+    int connection = connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    close(sockfd);
+    return connection;
+}
 typedef struct Task
 {
-    int a, b;
+    int port;
 } Task;
 
-Task taskQueue[256];
-int taskCount = 0;
+typedef struct TaskQueue
+{
+    Task *tasks;
+    int size;
+    int capacity;
+
+} TaskQueue;
+TaskQueue taskQueue;
+
 int terminate = 0;
 
 pthread_mutex_t mutex_queue;
 pthread_cond_t cond_queue;
 
+void init_task_queue(TaskQueue *queue, int capacity)
+{
+    queue->tasks = (Task *)malloc(capacity * sizeof(Task));
+    queue->size = 0;
+    queue->capacity = capacity;
+}
+
+void resize_task_queue(TaskQueue *queue)
+{
+    queue->capacity *= 2;
+    queue->tasks = (Task *)realloc(queue->tasks, queue->capacity * sizeof(Task));
+}
+
+void free_task_queue(TaskQueue *queue)
+{
+    free(queue->tasks);
+}
+
 void execute_task(Task *task)
-{   
-    usleep(50000);
+{
     // replace this with some long running operation
-    int result = task->a + task->b;
-    printf("Result: %d + %d = %d\n", task->a, task->b, result);
+    int connection = scan_port(task->port);
+    if (connection == 1)
+    {
+        printf("warn: error creating socket\n");
+        exit(1);
+    }
+
+    if (connection == 0)
+    {
+        printf("%s:%d/TCP: connection success\n", scanner_args.ip, task->port);
+    }
 }
 
 void submit_task(Task task)
 {
     pthread_mutex_lock(&mutex_queue);
-    taskQueue[taskCount] = task;
-    taskCount++;
+
+    if (taskQueue.size == taskQueue.capacity)
+    {
+        resize_task_queue(&taskQueue);
+    }
+    taskQueue.tasks[taskQueue.size++] = task;
     pthread_mutex_unlock(&mutex_queue);
     pthread_cond_signal(&cond_queue);
 }
@@ -49,22 +149,20 @@ void *start_thread(void *args)
         // critical section
         pthread_mutex_lock(&mutex_queue);
 
-        while (taskCount == 0 && !terminate)
+        while (taskQueue.size == 0 && !terminate)
         {
             pthread_cond_wait(&cond_queue, &mutex_queue);
         }
-        if(terminate && taskCount == 0){
+        if (terminate && taskQueue.size == 0)
+        {
+            // all jobs have been completed / claimed
             pthread_mutex_unlock(&mutex_queue);
             break;
         }
 
-        task = taskQueue[0];
-        int i;
-        for (i = 0; i < taskCount - 1; i++)
-        {
-            taskQueue[i] = taskQueue[i + 1];
-        }
-        taskCount--;
+        task = taskQueue.tasks[0];
+        memmove(&taskQueue.tasks[0], &taskQueue.tasks[1], (taskQueue.size - 1) * sizeof(Task));
+        taskQueue.size--;
         // end critical section
         pthread_mutex_unlock(&mutex_queue);
         execute_task(&task);
@@ -74,11 +172,8 @@ void *start_thread(void *args)
 
 int main(int argc, char *argv[])
 {
-    Task t1 = {
-        .a = 5,
-        .b = 10};
-
-    execute_task(&t1);
+    validate_args(argc, argv, &scanner_args);
+    init_task_queue(&taskQueue, INIT_QUEUE_CAPACITY);
     // create the thread pool
     pthread_t th[NUM_THREADS];
     pthread_mutex_init(&mutex_queue, NULL);
@@ -93,12 +188,10 @@ int main(int argc, char *argv[])
         }
     }
 
-    srand(time(NULL));
-    for (i = 0; i < 256; i++)
+    for (int i = scanner_args.start_port; i <= scanner_args.end_port; i++)
     {
         Task t = {
-            .a = rand() % 100,
-            .b = 10};
+            .port = i};
         submit_task(t);
     }
     pthread_mutex_lock(&mutex_queue);
@@ -114,7 +207,7 @@ int main(int argc, char *argv[])
     }
     pthread_mutex_destroy(&mutex_queue);
     pthread_cond_destroy(&cond_queue);
+    free_task_queue(&taskQueue);
     printf("Complete\n");
-
     return 0;
 }
